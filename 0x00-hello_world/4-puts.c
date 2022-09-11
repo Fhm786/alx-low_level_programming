1cb12cd1cab4ff206992dc641153462c3f2e05dc
Faheem <faheem.ebrahim786@gmail.com>
2022-09-11 14:55:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Desc : use main
-
+ *
  * this program prints "Programming is like building a multilingual puzzle
-
- * Return: Always 0 (Success)
-
+ *  Return: 0
  */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
-
+	return (0); 
 }
